05d91cfa6ca28dd2bcfc856c711a075d69eca387
AntaraAnika <anika@insurecow.com>
2022-06-14 16:00:14 +0600

    sis

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ for (let i = 0; i < splitText.length; i++) {
 }
 
 let char = 0;
-let timer = setInterval(onTick, 100);
+let timer = setInterval(onTick, 30);
 
 function onTick() {
     const span = text.querySelectorAll('span')[char];
